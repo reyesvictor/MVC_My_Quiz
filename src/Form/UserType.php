727fd677a3fc3e43479915f4ed32a0628ccffa61
Victor Reyes <victor.reyes@hotmail.fr>
2020-04-29 07:58:17 +0200

    Include of header on base.html + Before creating login form make:auth

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('email')
+            ->add('email_verified_at')
+            ->add('is_admin')
+            ->add('password')
+            ->add('remember_token')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('last_connected_at')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
